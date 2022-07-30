8070c215c9cd439589992a20b81f4129bd131f74
zuniversal <604688489@qq.com>
2022-07-30 21:16:11 +0800

    edit 修改样式

--- src/models/home.js
+++ src/models/home.js
@@ -4,7 +4,7 @@ import { nowYearMonthDayFull } from '@/utils';
 // import { powerConfigMap } from '@/pages/common/Home/PowerLineChart';
 
 const namespace = 'home';
-const { createAction, createDispatch, } = init(namespace);
+const { createAction, createDispatch } = init(namespace);
 
 const model = {
   namespace,
@@ -18,32 +18,36 @@ const model = {
   },
 
   reducers: {
-    getTemperatureHumidity(state, { payload, data, dtp, }) {
+    getTemperatureHumidity(state, { payload, data, dtp }) {
       console.log(' getTemperatureHumidity ： ', state, payload, data, dtp);
       return {
         ...state,
         temperatureHumidityInfo: data,
       };
     },
-    getElectricFee(state, { payload, data, }) {
-      console.log(' getElectricFee ： ', state, payload, data, );
+    getElectricFee(state, { payload, data }) {
+      console.log(' getElectricFee ： ', state, payload, data);
+      const c1 = { itemsTyle: { normal: { color: 'rgba(236, 78, 81)' } } };
+      const c2 = { itemsTyle: { normal: { color: 'rgba(231, 178, 69)' } } };
+      const c3 = { itemsTyle: { normal: { color: 'rgba(19, 208, 208)' } } };
+
       return {
         ...state,
         electricFee: {
           ep: [
-            { value: data.ep.valley, name: '谷时电量', },
-            { value: data.ep.usual, name: '平时电量', },
-            { value: data.ep.peak, name: '峰时电量', },
+            { value: data.ep.valley, name: '谷时电量', ...c1 },
+            { value: data.ep.usual, name: '平时电量', ...c2 },
+            { value: data.ep.peak, name: '峰时电量', ...c3 },
           ],
           fee: [
-            { value: data.fee.valley, name: '谷时电量', },
-            { value: data.fee.usual, name: '平时电量', },
-            { value: data.fee.peak, name: '峰时电量', },
-          ]
+            { value: data.fee.valley, name: '谷时电量', ...c1 },
+            { value: data.fee.usual, name: '平时电量', ...c2 },
+            { value: data.fee.peak, name: '峰时电量', ...c3 },
+          ],
         },
       };
     },
-    getPowerlineInfo(state, { payload, data, dtp, }) {
+    getPowerlineInfo(state, { payload, data, dtp }) {
       console.log(' getPowerlineInfo ： ', state, payload, data, dtp);
       // const powerlineInfo = {}
       // if (payload.query === powerConfigMap.POWER_CURVEif) {
@@ -58,7 +62,7 @@ const model = {
       // if (payload.query === powerConfigMap.POWER_LOADif) {
       //   // powerlineInfo = data
       // }
-      
+
       return {
         ...state,
         powerlineInfo: data,
@@ -74,38 +78,32 @@ const model = {
     ) {
       console.log(' getTemperatureHumidityAsync ： ', payload, action, type);
       const res = yield call(services.getTemperatureHumidity, payload);
-      console.log(' getTemperatureHumidityAsync resresres ： ', res,  )// 
+      console.log(' getTemperatureHumidityAsync resresres ： ', res); //
       yield put({
         type: 'getTemperatureHumidity',
         ...res,
         payload,
       });
     },
-    *getElectricFeeAsync(
-      { payload, action, type },
-      { call, put },
-    ) {
+    *getElectricFeeAsync({ payload, action, type }, { call, put }) {
       console.log(' getElectricFeeAsync ： ', payload, action, type);
       const res = yield call(services.getElectricFee, payload);
-      console.log(' getElectricFeeAsync resresres ： ', res,  )// 
+      console.log(' getElectricFeeAsync resresres ： ', res); //
       yield put({
         type: 'getElectricFee',
         ...res,
         payload,
       });
     },
-    *getPowerlineInfoAsync(
-      { payload, action, type },
-      { call, put },
-    ) {
+    *getPowerlineInfoAsync({ payload, action, type }, { call, put }) {
       console.log(' getPowerlineInfoAsync ： ', payload, action, type);
       const params = {
         start_time: `${nowYearMonthDayFull} 00:00:00`,
         end_time: `${nowYearMonthDayFull} 23:59:59`,
         ...payload,
-      }
+      };
       const res = yield call(services.getPowerlineInfo, params);
-      console.log(' getPowerlineInfoAsync resresres ： ', res,  )// 
+      console.log(' getPowerlineInfoAsync resresres ： ', res); //
       yield put({
         type: 'getPowerlineInfo',
         ...res,
--- src/pages/common/Home/PowerInstallLiquid/index.jsx
+++ src/pages/common/Home/PowerInstallLiquid/index.jsx
@@ -7,52 +7,60 @@ const configs = [
     title: '光伏 累计总发电量kWh',
     src: require('@/static/img/home/left/pv.png'),
     val: '689.460',
-    color: ['#31D4D5'],  
+    color: ['#31D4D5'],
     colorIntro: [
       {
-        class: 'introGreen', 
-        text: '实时发电功率', 
+        class: 'introGreen',
+        text: '实时发电功率',
       },
       {
-        class: 'introOrange', 
-        text: '总装机容量', 
+        class: 'introOrange',
+        text: '总装机容量',
       },
-    ],  
+    ],
   },
   {
     title: '储能 总装机容量kWh',
     src: require('@/static/img/home/left/storedEnergy.png'),
     val: '552',
-    color: ['#FC7154'],  
+    color: ['#FC7154'],
     colorIntro: [
       {
-        class: 'introGreen', 
-        text: '实时电量', 
+        class: 'introGreen',
+        text: '实时电量',
       },
       {
-        class: 'introOrange', 
-        text: '总装机容量', 
+        class: 'introOrange',
+        text: '总装机容量',
       },
     ],
   },
-]
+];
 
 const PowerInstallLiquid = props => {
-  return <div className="leftBox installCapacity">
-    {configs.map((item, index) => (<div className='flexBorderBox' key={index} >
-      <div className='iconTitle'>
-        <div className='df'>
-          <img src={item.src} className="icon"/>
-          <div className='chartTitle'>{item.title}</div>
+  return (
+    <div className="leftBox installCapacity">
+      {configs.map((item, index) => (
+        <div className="flexBorderBox installCapacityItem" key={index}>
+          <div className="iconTitle">
+            <div className="df">
+              <img src={item.src} className="icon" />
+              <div className="chartTitle">{item.title}</div>
+            </div>
+          </div>
+          <div className="chartVal">{item.val}</div>
+          <PowerLiquid {...item} {...props}></PowerLiquid>
+          <div className="colorIntroWrapper">
+            {item.colorIntro.map((v, i) => (
+              <div className={`colorIntroItem ${v.class}`} key={i}>
+                {v.text}
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-      <div className='chartVal'>{item.val}</div>
-      <PowerLiquid {...item} {...props} ></PowerLiquid>
-      <div className='colorIntroWrapper'>
-        {item.colorIntro.map((v, i) => (<div className={`colorIntroItem ${v.class}`} key={i} >{v.text}</div>))}
-      </div>
-    </div>))}
-  </div>
+      ))}
+    </div>
+  );
 };
 
 PowerInstallLiquid.defaultProps = {};
